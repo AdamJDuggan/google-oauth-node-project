8df5e8de067a946ca55441b621c9fefea52dd393
Adam Duggan <AdamD@here.local>
2019-07-30 16:20:35 +0100

    google oauth2 working

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI: 'mongodb://AdamJDuggan:Munster1@ds151169.mlab.com:51169/storybooks-dev',
+  googleClientID: '787277580701-6kpm65v0tlg0vqgnbfrs3vd94lr698t8.apps.googleusercontent.com',
+  googleClientSecret: '_xyd1QArHzIIktSJeyNkruAa'
+}
